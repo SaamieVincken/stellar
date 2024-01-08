4e1a36cef28d8a883a70fd8ed3f5b52482ecdc1a
SaamieVincken <saamievincken@protonmail.com>
2024-01-08 14:40:47 +0100

    Number to decimal

--- src/lib/shared/Color/Color.js
+++ src/lib/shared/Color/Color.js
@@ -1,17 +1,15 @@
 import {spectralTypeToHSL} from "$lib/shared/Color/HSL/GenerateHSL.js";
 import {CalculateWavelength} from "$lib/shared/Color/WavelenghtCalc.js";
-import {HSLtoRGB} from "$lib/shared/Color/HSL/HSLtoRGB.js";
 
 /**
  *
- * @param {number} mass in solar Masses (M☉)
- * @param {number} luminosity in solar Luminosity (L☉)
- * @param {number} temperature in Kelvin
- * @return {number[]} RGB color value
+ * @param {Decimal} mass in solar Masses (M☉)
+ * @param {Decimal} luminosity in solar Luminosity (L☉)
+ * @param {Decimal} temperature in Kelvin
+ * @return {Decimal[]} RGB color value
  * @constructor
  */
-export function GetColor(mass, luminosity, temperature){
+export function GetColor(mass, luminosity, temperature) {
     let wavelength = CalculateWavelength(temperature);
-    let hsl = spectralTypeToHSL(luminosity, wavelength, temperature);
-    return HSLtoRGB(hsl);
+    return spectralTypeToHSL(luminosity, wavelength, temperature);
 }
--- src/lib/shared/StellarProperties/EffectiveTemperature.js
+++ src/lib/shared/StellarProperties/EffectiveTemperature.js
@@ -1,17 +1,26 @@
-/**
- * @type {number} Stefan-Boltzmann constant in W/m²·K⁴
- * @type {number} pi
- */
-const stefanBoltzmannConstant = 5.67e-8;
-const pi = Math.PI;
+import Decimal from 'decimal.js';
+
+// Stefan-Boltzmann constant in W/m²·K⁴
+const stefanBoltzmannConstant = new Decimal('5.67e-8');
+
+// Pi constant
+const pi = new Decimal(Math.PI);
 
 /**
  * Calculate the effective temperature using Teff = (L/(4πR^2σ))^1/4
- * @param {number} luminosity in solar luminosity (L☉)
- * @param {number} radius in meters (m)
- * @return {number} in Kelvin (K)
+ * @param {Decimal} luminosity in solar luminosity (L☉)
+ * @param {Decimal} radius in meters (m)
+ * @return {Decimal} in Kelvin (K)
  */
-export function calculateEffectiveTemperature(luminosity, radius){
-    return Math.pow((luminosity / (4 * pi * Math.pow(radius, 2) * stefanBoltzmannConstant)), 0.25);
-}
+export function calculateEffectiveTemperature(luminosity, radius) {
+    luminosity = new Decimal(luminosity);
+    radius = new Decimal(radius);
 
+    // Perform the calculation using Decimal methods
+    return Decimal.pow(
+        luminosity.div(
+            Decimal.mul(4, pi, radius.pow(2), stefanBoltzmannConstant)
+        ),
+        0.25
+    );
+}
