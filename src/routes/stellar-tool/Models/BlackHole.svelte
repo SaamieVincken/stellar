a6ed51f5de74b469c8819449f794cb57240fb1ea
SaamieVincken <saamievincken@protonmail.com>
2024-01-08 14:40:35 +0100

    Add black hole model

---
+++ src/routes/stellar-tool/Models/BlackHole.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from 'svelte';
+    import { initializeBlackHole } from "$lib/shared/Models/BlackHoleModel.js";
+    import {disposeStar} from "$lib/shared/Models/StarModel.js";
+
+    onMount(() => {
+        initializeBlackHole();
+    });
+</script>
+
+<style>
+    #renderer-container{
+
+    }
+</style>
+
+<div id="renderer-container"></div>
