0ab39ec339db2193e178f8f076c3303479ecaff8
SaamieVincken <saamievincken@protonmail.com>
2024-01-08 14:44:25 +0100

    Add star store

--- src/lib/shared/SetStar.js
+++ src/lib/shared/SetStar.js
@@ -1,15 +1,16 @@
-import { writable } from 'svelte/store';
+
 import { Star } from "$lib/shared/StarClass.js";
 import {GetPhases} from "$lib/shared/Phase/PhaseCalc.js";
+import {star} from "$lib/shared/StarStore.js";
 
-export const star = writable(undefined);
-export const phase = writable(undefined);
-
+/**
+ *
+ * @param solarMass
+ * @param solarLuminosity
+ * @param surfaceTemperature
+ */
 export function setStar(solarMass, solarLuminosity, surfaceTemperature) {
-    let newPhase = GetPhases(solarMass, solarLuminosity, surfaceTemperature);
-    phase.set(newPhase);
-
+    let newPhase = GetPhases(solarLuminosity, solarMass, surfaceTemperature);
     const newStar = new Star(newPhase, solarMass, solarLuminosity, surfaceTemperature);
     star.set(newStar);
-    console.log(newStar);
 }
--- src/lib/shared/StarStore.js
+++ src/lib/shared/StarStore.js
@@ -1,4 +1,3 @@
 import { writable } from 'svelte/store';
 
 export const star = writable( undefined );
-export const phase = writable(undefined);
