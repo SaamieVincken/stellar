5069d232672c1c4a5f4b2ec28c3ccef9d8dc7f49
SaamieVincken <saamievincken@protonmail.com>
2024-01-08 14:45:18 +0100

    Add supernova model

---
+++ src/routes/stellar-tool/Models/Supernova.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+    import {initializeSupernova} from "$lib/shared/Models/SupernovaModel.js";
+
+    onMount(() => {
+        initializeSupernova();
+    });
+</script>
+
+<style>
+    #renderer-container {
+    }
+</style>
+
+<div id="renderer-container"></div>
