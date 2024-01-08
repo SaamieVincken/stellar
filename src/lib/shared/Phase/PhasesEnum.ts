405feee9cdef572bc9e66e902f929a60222df916
SaamieVincken <saamievincken@protonmail.com>
2024-01-08 14:44:11 +0100

    Add detailed information about phases

--- src/lib/shared/Phase/PhasesEnum.ts
+++ src/lib/shared/Phase/PhasesEnum.ts
@@ -1,11 +1,12 @@
 export enum PhasesEnum {
     ProtoStar = 'Proto Star',
     RedDwarf = 'Red Dwarf',
-    YellowGiant = 'YellowGiant',
+    YellowDwarf = 'Yellow Dwarf',
     BlueGiant = 'Blue Giant',
     RedGiant = 'Red Giant',
     Supernova = 'Supernova',
     NeutronStar = 'Neutron Star',
     BlackHole = 'Black Hole',
-    WhiteDwarf = 'White Dwarf'
+    WhiteDwarf = 'White Dwarf',
+    Unknown = 'Unknown',
 }
--- src/routes/stellar-tool/PhaseInformation.svelte
+++ src/routes/stellar-tool/PhaseInformation.svelte
@@ -1,52 +1,136 @@
+<style>
+    .mt-extraTitle {
+        margin-top: 280px;
+    }
+    .mt-extra {
+        margin-top: 350px;
+    }
+
+    .custom-border{
+        border: #000d21;
+    }
+
+    .supernova-color {
+        color: #201d82;
+    }
+
+    .nebula-color {
+        color: #94B3FD;
+    }
+
+    .unknown-color {
+        color: #5b4278;
+    }
+
+    .yellow-dwarf-color {
+        color: #c4c27e;
+    }
+</style>
+
 <script>
     import TimeBar from "./TimeBar.svelte";
-    //import { BlackHole } from "$lib/shared/Phase/StellarObjects.js";
-    import StarModel from "../stellar-models/StarModel.svelte";
-    import { onMount, onDestroy } from 'svelte';
-    import * as THREE from "three";
-    import {initializeStar} from "$lib/shared/StarModel.js";
+    import StarModel from "./Models/StarModel.svelte";
     import { star } from '$lib/shared/StarStore.js';
-
-    let phase = "Unknown";
+    import Supernova from "./Models/Supernova.svelte";
+    import BlackHole from "./Models/BlackHole.svelte";
+    import Nebula from "./Models/Nebula.svelte";
+    import {
+        LuminosityClassPercentage
+    } from "$lib/shared/SpectralClassification/LuminosityClass/LuminosityPercentage";
+    let luminosityClassKey;
     let starData = {};
+    $: if ($star) {
+        console.log("Reactive $star update:", $star);
+        luminosityClassKey = getKeyByValue(LuminosityClassPercentage, starData.luminosityClass);
+        starData = $star;
 
-    const unsubscribe = star.subscribe(value => {
-        starData = value;
-    });
-
-    onMount(() => {
-        initializeStar(new THREE.Color(0xD8BFD8), 1.6, 1);
-    });
-
-    onDestroy(() => {
-        unsubscribe();
-    });
+    }
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key] === value);
+    }
 </script>
-
 <div class="fixed flex items-center justify-center w-full">
-    <div class="-mt-52">
-            <StarModel/>
-    </div>
-</div>
-
-<div>
-    <TimeBar/>
-</div>
-
-<div class="fixed flex items-center justify-center w-full">
-    <div class="mt-80 w-1/2 text-white">
-        {#if phase.phase !== undefined}
-            <!--{#if isBlackHole}-->
-            <!--    <h2 class="font-bold">{phase.phase}</h2>-->
-            <!--    <p>{phase.mass}</p>-->
-            <!--{:else}-->
-            <!--    <h2 class="font-bold">{phase.phase}</h2>-->
-            <!--    <p>{phase.luminosity}</p>-->
-            <!--    <p>{phase.mass}</p>-->
-            <!--    <p>{phase.temperature}</p>-->
-            <!--    <p>{phase.phaseDuration}</p>-->
-            <!--    <p>{phase.diameter}</p>-->
-            <!--{/if}-->
+    <div class="-mt-52" style="position: relative;">
+        {#if starData.phase !== undefined}
+            {#if starData.phase === "Supernova"}
+                <Supernova/>
+            {:else if starData.phase === "Black Hole"}
+                <BlackHole/>
+            {:else if starData.phase === "Unknown"}
+                <Nebula/>
+            {:else}
+                <StarModel/>
+            {/if}
+        {:else}
+            <Nebula/>
         {/if}
     </div>
 </div>
+
+
+<div>
+    <TimeBar/>
+    <div class="fixed flex items-center justify-center w-full">
+        <div class="mt-extraTitle w-1/2 text-white">
+            {#if starData.phase !== undefined}
+                {#if starData.phase !== "Black Hole" }
+                    <h1 class="ml-1 text-2xl md:text-3xl font-semibold tracking-tight font-mono
+                {starData.phase === 'Supernova' ? 'supernova-color' :
+                 starData.phase === 'Nebula' ? 'nebula-color' :
+                 starData.phase === 'Unknown' ? 'unknown-color' :
+                 starData.phase === 'Yellow Dwarf' ? 'yellow-dwarf-color' : ''}"
+                    >
+                        {starData.phase}
+                    </h1>
+                    {:else}
+                    <h1 class="ml-1 text-2xl md:text-3xl font-semibold tracking-tight font-sans text-red-800">
+                        {starData.phase}
+                    </h1>
+                {/if}
+
+            {/if}
+        </div>
+    </div>
+
+    <div class="fixed flex items-center justify-center w-full">
+        <div class="mt-extra w-1/2 text-white bg-black border-2 custom-border font-mono">
+            {#if starData.phase !== undefined}
+                {#if starData.phase !== "Black Hole" }
+                    <div class="flex justify-between">
+                        <div>
+                            <p class="ml-1">solar masses: {starData.solarMass} M☉</p>
+                            <p class="ml-1">luminosity: {starData.solarLuminosity} L☉</p>
+                            <p class="ml-1">temperature: {starData.surfaceTemperature} Kelvin</p>
+                        </div>
+                            <div>
+                            <p class="ml-1">radius: {Math.round(starData.stellarRadius / 1000).toLocaleString('de-DE')} km</p>
+                            <p class="ml-1">density: {starData.stellarDensity.toExponential(2)} g/cm³</p>
+                            <p class="ml-1 mb-2">volume: {Number(starData.stellarVolume).toExponential(2)} m³</p>
+
+                        </div>
+                        <div>
+                            <p class="ml-1 text-blue-500">spectral type: {starData.spectralType}</p>
+                            <p class="ml-1 text-blue-500">luminosity class: {luminosityClassKey}</p>
+                        </div>
+                    </div>
+                {:else}
+                    <p class="ml-1 mt-2">solar masses: {starData.solarMass} M☉</p>
+                    <p class="ml-1">temperature: {starData.surfaceTemperature} Kelvin</p>
+                {/if}
+            {/if}
+        </div>
+    </div>
+</div>
+
+<!--    phase;
+    solarMass;
+    solarLuminosity;
+    surfaceTemperature;
+    effectiveTemperature;
+    stellarRadius;
+    stellarDensity;
+    spectralType;
+    luminosityClass;
+    hslColor;-->
+
+
